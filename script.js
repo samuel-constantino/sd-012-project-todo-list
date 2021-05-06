540112a2bc5c97bb15e7268a75fe4e7bfb30f2c5
Samuel Constantino <samuelicapuidados@gmail.com>
2021-05-06 17:39:30 -0300

    implementa requisito 9: marcação de item com duplo clique

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// adiciona nova tarefa à lista
 function addTarefa(olTarefas) {
   // acessa elemento <button id='criar-tarefa'>Criar</button>
   const btnCriarTarefa = document.querySelector('#criar-tarefa');
@@ -18,35 +19,46 @@ function addTarefa(olTarefas) {
   });
 }
 
-// adiciona evento para itens da lista
-function eventoParaItensDaLista(olTarefas) {
+
+function eventoDeSelecaoDeItemDaLista(olTarefas) {
   olTarefas.addEventListener('click', (event) => {
     // se o alvo clicado for uma tarefa
     if (event.target.classList.contains('tarefa')) {
-      // remove classe 'selecteed' do item anterior
-      for (let tarefa of olTarefas.querySelectorAll('.tarefa')) {
-        tarefa.classList.remove('selected');
-
+      // remove classe 'selected' do item anterior
+      let tarefaSelecionadaAnterior = olTarefas.querySelector('.selected');
+      // se for a primeira seleção
+      if(tarefaSelecionadaAnterior != null){
+        tarefaSelecionadaAnterior.classList.remove('selected');
       }
-      // adiciona classe 'selected'
+      // seleciona o alvo
       event.target.classList.add('selected');
+      // adiciona fundo branco em todos itens
       for (let tarefa of olTarefas.querySelectorAll('.tarefa')) {
-        // adiciona background personalizado na classe 'selected'
-        if (tarefa.className === 'tarefa selected') {
-          tarefa.style.backgroundColor = 'rgb(128, 128, 128)';
-        } else {
-          tarefa.style.backgroundColor = 'rgb(255, 255, 255)';
-        }
+        tarefa.style.backgroundColor = 'rgb(255, 255, 255)';
+      }
+      // destaca item selecionado
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+  });
+}
+function eventoDeMarcacaoDeItemDaLista(olTarefas) {
+  olTarefas.addEventListener('dblclick', (event) => {
+    // se o alvo for uma tarefa
+    if(event.target.classList.contains('tarefa')){
+      // verificar se tarefa está completa
+      if(event.target.classList.contains('completed')){
+        event.target.classList.remove('completed');
+      } else {
+        event.target.classList.add('completed');
       }
     }
   });
 }
-
 // garante que o html estará carregado
 window.onload = () => {
   // acessa elemento <ol id='lista-tarefas'></ol>
   let olTarefas = document.querySelector('#lista-tarefas');
   addTarefa(olTarefas);
-  eventoParaItensDaLista(olTarefas);
-
+  eventoDeSelecaoDeItemDaLista(olTarefas);
+  eventoDeMarcacaoDeItemDaLista(olTarefas)
 }
