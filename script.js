6146384477f1ccd372527017bdf8ccf44ffa7e9a
Samuel Constantino <samuelicapuidados@gmail.com>
2021-05-07 15:13:51 -0300

    refatora botões para cima e para baixo: resolve erro de elemento selecionado indefinido

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function verificaTarefasSalvas(olTarefas) {
     // converte classes para array
     let classes = classesEmString.split(',');
     // popular a lista de tarefas
-    for(let key in lista){
+    for (let key in lista) {
       // cria elemento <li>
       let tarefa = document.createElement('li');
       //  adiciona classe 'tarefa'
@@ -130,15 +130,19 @@ function eventoMoverBaixo(olTarefas) {
     let tarefas = olTarefas.getElementsByTagName('li');
     // identifica qual elemento é o selecionado
     let elementSelected;
-    for(let tarefa of tarefas) {
-      if(tarefa.classList.contains('selected')){
+    for (let tarefa of tarefas) {
+      if (tarefa.classList.contains('selected')) {
         elementSelected = tarefa;
       }
     }
-    // se o próximo elemento for diferente de nulo, subistitua
-    if(elementSelected.nextElementSibling != null){
-      olTarefas.insertBefore(elementSelected.nextElementSibling, elementSelected);
+    // se houver um item selecionado
+    if (elementSelected != undefined) {
+      // se o próximo elemento for diferente de nulo, subistitua
+      if (elementSelected.nextElementSibling != null) {
+        olTarefas.insertBefore(elementSelected.nextElementSibling, elementSelected);
+      }
     }
+
   });
 }
 
@@ -149,14 +153,17 @@ function eventoMoverCima(olTarefas) {
     let tarefas = olTarefas.getElementsByTagName('li');
     // identifica qual elemento é o selecionado
     let elementSelected;
-    for(let tarefa of tarefas) {
-      if(tarefa.classList.contains('selected')){
+    for (let tarefa of tarefas) {
+      if (tarefa.classList.contains('selected')) {
         elementSelected = tarefa;
       }
     }
-    // se o próximo elemento for diferente de nulo, subistitua
-    if(elementSelected.previousElementSibling != null){
-      elementSelected.after(elementSelected.previousElementSibling);
+    // se houver um item selecionado
+    if (elementSelected != undefined) {
+      // se o próximo elemento for diferente de nulo, subistitua
+      if (elementSelected.previousElementSibling != null) {
+        elementSelected.after(elementSelected.previousElementSibling);
+      }
     }
   });
 }
@@ -169,13 +176,13 @@ function eventoRemoverTarefaSelecionada(olTarefas) {
     // acessa lista de tarefas
     let tarefas = olTarefas.querySelectorAll('.tarefa');
     // acessa elemento selecionado
-    for(let tarefa of tarefas) {
-      if(tarefa.classList.contains('selected')){
+    for (let tarefa of tarefas) {
+      if (tarefa.classList.contains('selected')) {
         olTarefas.removeChild(tarefa);
         break;
       }
     }
-    
+
   });
 }
 
